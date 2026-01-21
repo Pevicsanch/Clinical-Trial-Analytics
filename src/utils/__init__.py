751486dee17f3a2e24450fd34880f15cf9a4f9e8
pevic sanch <sanchez.pv@pm.me>
2026-01-21 01:46:25 +0100

    refactor(utils): export notebook helpers from package __init__

---
+++ src/utils/__init__.py
@@ -0,0 +1,15 @@
+"""
+Utility modules for clinical trial analysis.
+"""
+
+from src.utils.notebook import (
+    find_project_root,
+    check_dependencies,
+    register_shared_vars,
+)
+
+__all__ = [
+    "find_project_root",
+    "check_dependencies",
+    "register_shared_vars",
+]
